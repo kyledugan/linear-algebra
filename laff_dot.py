3e092dd8c279175372ea384c666346064f9d0866
Kyle Dugan <kyle.dugan@merck.com>
2020-04-16 15:02:07 -0500

    Add laff_dot.py

---
+++ laff_dot.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def laff_dot(x,y):
+    # check that 2D arrays are passed in
+    if len(x.shape) != 2 or len(y.shape) != 2:
+        return 'FAILED'
+        
+    mx, nx = x.shape
+    my, ny = y.shape
+    dot = 0
+    
+    # check that both inputs are vectors
+    if (mx != 1 and nx != 1) or (my != 1 and ny != 1):
+        return 'FAILED'
+    # check that both vectors have same number of elements
+    if mx * nx != my * ny:
+        return 'FAILED'
+    
+    if mx == 1 and my == 1: # both are row vectors
+        for i in range(nx):
+            dot += x[0][i] * y[0][i]
+    elif nx == 1 and ny == 1: # both are column vectors
+        for i in range(mx):
+            dot += x[i][0] * y[i][0]
+    elif my == 1: # x is a column vector and y is a row vector
+        for i in range(mx):
+            dot += x[i][0] * y[0][i]
+    else: # x is a row vector and y is a column vector
+        for i in range(nx):
+            dot += x[0][i] * y[i][0]
+
+    return dot
+
+x = np.array([[1,2,3]])
+f1 = np.array([[4,5,6], [-1,-2,-3]])
+f2 = np.array([[4,5]])
+y = np.array([[7],[8],[9]])
+z = np.array([[10], [11], [12]])
+print(laff_dot(x,f1))

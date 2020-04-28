881d6e9a22a635afda86644f16a379f245faa3bc
Kyle Dugan <kyle.dugan@merck.com>
2020-04-28 10:13:42 -0600

    Add upper_triangle_matrix_vector_mult.py

---
+++ week4/upper_triangle_matrix_vector_mult.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sys
+sys.path.append('../week1')
+from dot import dot
+
+def transpose_matrix_vector_mult(A,x,y):
+    # check that all inputs are 2x2 arrays
+    if len(A.shape) != 2 or len(x.shape) != 2 or len(y.shape) != 2:
+        return "FAILED"
+    # check that x and y are both column vectors
+    if y.shape[1] != 1 or x.shape[1] != 1:
+        return "FAILED"
+    # check that inputs have compatible dimensions
+    if y.shape[0] != x.shape[0] or A.shape[1] != x.shape[0]:
+        return "FAILED"
+
+    for i in range(A.shape[1]):
+        y[i,0] += dot(np.array([A[i,i:]]), x[i:])
+    
+    return y
+
+A = np.array([[1,-1,3,2,-2], [0,-2,1,0,-1], [0,0,3,2,1], [0,0,0,1,0], [0,0,0,0,-2]])
+x = np.array([[-1], [0], [2], [-1], [1]])
+y = np.array([[0], [0], [0], [0], [0]])
+print(transpose_matrix_vector_mult(A,x,y))

6085941007d7ad843eca8e753f7cb2265152f31e
Kyle Dugan <kyle.dugan@merck.com>
2020-05-17 09:57:14 -0600

    Split substitution into forward and backward pieces

--- week6/forward_substitution.py
+++ week6/forward_substitution.py
@@ -1,16 +1,14 @@
 import numpy as np
 
-def back_substitution(A, b):
+def forward_substitution(A, b):
     if len(A.shape) != 2 or len(b.shape) != 2:
         return "FAILED"
     if A.shape[0] != A.shape[1]:
         return "FAILED"
     if b.shape[0] != A.shape[0] or b.shape[1] != 1:
         return "FAILED"
+    
+    for i in range(A.shape[0]):
+        b[i+1:,0] -= b[i,0] * A[i+1:,i]
 
-    for i in range(A.shape[0]-1, -1, -1):
-        div = b[i,0]
-        for j in range(i+1, A.shape[0]):
-            div -= A[i,j] * b[j,0]
-        b[i,0] = div / A[i,i]
     return b
--- week6/solve_Ax_eq_b.py
+++ week6/solve_Ax_eq_b.py
@@ -1,9 +1,11 @@
 import numpy as np
 from gaussian_elimination import gaussian_elimination
+from forward_substitution import forward_substitution
 from back_substitution import back_substitution
 
 def solve_Ax_eq_b(A, b):
     A = gaussian_elimination(A)
+    b = forward_substitution(A, b)
     return back_substitution(A, b)
 
 A = np.array([[2,0,1,2], [-2,-1,1,-1], [4,-1,5,4], [-4,1,-3,-8]])
